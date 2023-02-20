f891ad5dd5276792b3b42ded75038bc627fa4e65
Ayman-Al-Khatib <king1aeman@gmail.com>
2023-02-20 05:31:24 -0800

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+void main(List<String> args) {
+  runApp(NotesApp());
+}
+
+class NotesApp extends StatelessWidget {
+  const NotesApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+
+    );
+  }
+}
